cb87e5bcc4d75025355b70b89208513ff5cff4ae
Guillaume Perrin <gperrin01@gmail.com>
2015-05-10 16:30:48 +0200

    structuring the page look

--- scripts/main_with_jQuery.js
+++ scripts/main_with_jQuery.js
@@ -1,28 +1,25 @@
-soundManager.setup({ url: './SM2/swf/' });
+// soundManager.setup({ url: './SM2/swf/' });
   $(document).ready(function() {
   setUpEventListeners()
 })
-soundManager.onload = function(){
-  soundManager.createSound()
-}
+// soundManager.onload = function(){
+//   soundManager.createSound()
+// }
 
 /* Event Listeners  */
 var setUpEventListeners = function() {
   $('.greeting').on('click', function() {
-    showGreeting($(this).attr('id'));
-    italian.play();
+    console.log()
 })
 } /* end Listeners  */
 
 /* Functions linked to Listeners  */
-function showGreeting (language) {
-  $('#output-text').text('WDI 13 speaks: ' + language);
-}
+
 /* end Functions linked to listeners */
 
 /* All sounds */
-var wow_papa = soundManager.createSound({ url: './audiofiles/italian.mp3'});
-var italian = soundManager.createSound({ url: './audiofiles/wow_papa.m4a'});
+// var wow_papa = soundManager.createSound({ url: './audiofiles/italian.mp3'});
+// var italian = soundManager.createSound({ url: './audiofiles/wow_papa.m4a'});
 // End sounds declarations
 
 
