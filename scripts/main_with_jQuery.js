fd9ad4580a6d0f05255192fa3f1bd6ae86be34b1
Guillaume Perrin <gperrin01@gmail.com>
2015-05-11 10:06:46 +0100

    all is here, but soundManager sounds dont fire

--- scripts/main_with_jQuery.js
+++ scripts/main_with_jQuery.js
@@ -8,9 +8,31 @@
 
 /* Event Listeners  */
 var setUpEventListeners = function() {
-  $('.greeting').on('click', function() {
-    console.log()
-})
+  $('#pictop1').on('click', function() {
+    console.log( 'opapa_clap.play()' )
+  })
+  $('#pictop2').on('click', function() {
+    console.log( 'vpapa_intro.play()' )
+  })
+  $('#pictop3').on('click', function() {
+    console.log( 'woow_call_maybe()' )
+  })
+  $('#cake').on('click', function() {
+    console.log( 'coldplay_cake.play()' )
+  })
+  $('#pictop4').on('click', function() {
+    console.log( 'bravo.play()' )
+  })
+  $('#pictop5').on('click', function() {
+    console.log( 'habibi_clap.play()' )
+  })
+  $('#pictop6').on('click', function() {
+    console.log( 'Omarpapa_laugh.play()' )
+  })
+  $('#happy').on('click', function() {
+    console.log( 'happy.togglePause()' )
+  })
+
 } /* end Listeners  */
 
 /* Functions linked to Listeners  */
@@ -18,8 +40,14 @@ var setUpEventListeners = function() {
 /* end Functions linked to listeners */
 
 /* All sounds */
-// var wow_papa = soundManager.createSound({ url: './audiofiles/italian.mp3'});
-// var italian = soundManager.createSound({ url: './audiofiles/wow_papa.m4a'});
+var opapa_clap = soundManager.createSound({ url: './audio/opapa_clap.m4a'});
+var vpapa_intro = soundManager.createSound({ url: './audio/vpapa_intro.m4a'});
+var woow_call_maybe = soundManager.createSound({ url: './audio/woow_call_maybe.m4a'});
+var coldplay_cake = soundManager.createSound({ url: './audio/coldplay_cake.m4a'});
+var bravo = soundManager.createSound({ url: './audio/bravo.m4a'});
+var habibi_clap = soundManager.createSound({ url: './audio/habibi_clap.m4a'});
+var Omarpapa_laugh = soundManager.createSound({ url: './audio/Omarpapa_laugh.m4a'});
+var happy = soundManager.createSound({ url: './audio/happy.m4a'});
 // End sounds declarations
 
 
